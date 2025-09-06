33bab450781360721862fdeb85273ae8bdad2988
Rahu <rahu.exp@gmail.com>
2025-09-06 23:33:33 +0600

    Set allowed host for pythonAnywhere.

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'ewpis--2nm&$8fo^#q8z+-th_6hqrnnv$1@-5!tf2k19ld1vyz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['rahu.pythonanywhere.com']
 
 
 # Application definition
